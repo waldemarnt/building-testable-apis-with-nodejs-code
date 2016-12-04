1114a7e9797ccb70155dabe67e6b431c87ef8e88
Waldemar Neto <waldemarnt@gmail.com>
2016-12-04 17:48:50 -0200

    Adds green test state

--- src/app.js
+++ src/app.js
@@ -5,5 +5,10 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/products', (req, res) => res.send([{
+  name: 'Default product',
+  description: 'product description',
+  price: 100
+}]));
 
 export default app;
