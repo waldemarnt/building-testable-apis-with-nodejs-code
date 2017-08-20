ecc1425c099c2154202452771dbb100dd76e9b8f
Waldemar Neto <waldemarnt@gmail.com>
2017-08-20 20:21:42 -0300

    CRUD operations

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -10,6 +10,7 @@ describe('Routes: Products', () => {
       });
   });
 
+  const defaultId = '56cb91bdc3464f14678934ca';
   const defaultProduct = {
     name: 'Default product',
     description: 'product description',
@@ -17,11 +18,11 @@ describe('Routes: Products', () => {
   };
   const expectedProduct = {
     __v: 0,
-    _id: '56cb91bdc3464f14678934ca',
+    _id: defaultId,
     name: 'Default product',
     description: 'product description',
     price: 100
-  };
+  }
 
   beforeEach(() => {
     const product = new Product(defaultProduct);
@@ -41,6 +42,78 @@ describe('Routes: Products', () => {
           expect(res.body).to.eql([expectedProduct]);
           done(err);
         });
+    });
+
+    context('when an id is specified', done => {
+      it('should return 200 with one product', done => {
+
+        request
+          .get(`/products/${defaultId}`)
+          .end((err, res) => {
+            expect(res.statusCode).to.eql(200);
+            expect(res.body).to.eql([expectedProduct]);
+            done(err);
+          });
+      });
+    });
+  });
+
+  describe('POST /products', () => {
+    context('when posting a product', () => {
+
+      it('should return a new product with status code 201', done => {
+        const customId = '56cb91bdc3464f14678934ba';
+        const newProduct = Object.assign({},{ _id: customId, __v:0 }, defaultProduct);
+        const expectedSavedProduct = {
+          __v: 0,
+          _id: customId,
+          name: 'Default product',
+          description: 'product description',
+          price: 100
+        };
+
+        request
+          .post('/products')
+          .send(newProduct)
+          .end((err, res) => {
+            expect(res.statusCode).to.eql(201);
+            expect(res.body).to.eql(expectedSavedProduct);
+            done(err);
+          });
+      });
+    });
+  });
+
+  describe('PUT /products/:id', () => {
+    context('when editing a product', () => {
+      it('should update the product and return 200 as status code', done => {
+        const customProduct = {
+          name: 'Custom name'
+        };
+        const updatedProduct = Object.assign({}, customProduct, defaultProduct)
+
+        request
+          .put(`/products/${defaultId}`)
+          .send(updatedProduct)
+          .end((err, res) => {
+            expect(res.status).to.eql(200);
+            done(err);
+          });
+      });
+    });
+  });
+
+  describe('DELETE /products/:id', () => {
+    context('when deleting a product', () => {
+      it('should delete a product and return 204 as status code', done => {
+
+        request
+          .delete(`/products/${defaultId}`)
+          .end((err, res) => {
+            expect(res.status).to.eql(204);
+            done(err);
+          });
+      });
     });
   });
 });
--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -2,16 +2,21 @@ import ProductsController from '../../../src/controllers/products';
 import sinon from 'sinon';
 import Product from '../../../src/models/product';
 
-describe('Constrollers: Products', () => {
+describe('Controller: Products', () => {
   const defaultProduct = [{
+    __v: 0,
+    _id: "56cb91bdc3464f14678934ca",
     name: 'Default product',
     description: 'product description',
     price: 100
   }];
 
+  const defaultRequest = {
+    params: {}
+  };
+
   describe('get() products', () => {
     it('should call send with a list of products', () => {
-      const request = {};
       const response = {
         send: sinon.spy()
       };
@@ -21,7 +26,7 @@ describe('Constrollers: Products', () => {
 
       const productsController = new ProductsController(Product);
 
-      return productsController.get(request, response)
+      return productsController.get(defaultRequest, response)
         .then(() => {
           sinon.assert.calledWith(response.send, defaultProduct);
         });
@@ -45,5 +50,209 @@ describe('Constrollers: Products', () => {
           sinon.assert.calledWith(response.send, 'Error');
         });
     });
+
+  });
+
+  describe('getById()', () => {
+    it('should call send with one product', () => {
+      const fakeId = 'a-fake-id';
+      const request = {
+        params: {
+          id: fakeId
+        }
+      };
+      const response = {
+        send: sinon.spy()
+      };
+
+      Product.find = sinon.stub();
+      Product.find.withArgs({ _id: fakeId }).resolves(defaultProduct);
+
+      const productsController = new ProductsController(Product);
+
+      return productsController.getById(request, response)
+        .then(() => {
+          sinon.assert.calledWith(response.send, defaultProduct);
+        });
+    });
+  });
+
+  describe('create() product', () => {
+    it('should call send with a new product', () => {
+      const requestWithBody = Object.assign({}, { body: defaultProduct[0] }, defaultRequest);
+      const response = {
+        send: sinon.spy(),
+        status: sinon.stub()
+      };
+      class fakeProduct {
+        save() {}
+      }
+
+      response.status.withArgs(201).returns(response);
+      sinon.stub(fakeProduct.prototype, 'save').withArgs().resolves();
+
+      const productsController = new ProductsController(fakeProduct);
+
+      return productsController.create(requestWithBody, response)
+        .then(() => {
+          sinon.assert.calledWith(response.send);
+        });
+    });
+
+    context('when an error occurs', () => {
+      it('should return 422', () => {
+        const response = {
+          send: sinon.spy(),
+          status: sinon.stub()
+        };
+
+        class fakeProduct {
+          save() {}
+        }
+
+        response.status.withArgs(422).returns(response);
+        sinon.stub(fakeProduct.prototype, 'save').withArgs().rejects({ message: 'Error' });
+
+        const productsController = new ProductsController(fakeProduct);
+
+        return productsController.create(defaultRequest, response)
+          .then(() => {
+            sinon.assert.calledWith(response.status, 422);
+          });
+      });
+    });
+  });
+
+  describe('update() product', () => {
+    it('should respond with 200 when the product has been updated', () => {
+      const fakeId = 'a-fake-id';
+      const updatedProduct = {
+        _id: fakeId,
+        name: 'Updated product',
+        description: 'Updated description',
+        price: 150
+      };
+      const request = {
+        params: {
+          id: fakeId
+        },
+        body: updatedProduct
+      };
+      const response = {
+        sendStatus: sinon.spy()
+      };
+
+      class fakeProduct {
+        static findOneAndUpdate() {}
+      }
+
+      const findOneAndUpdateStub = sinon.stub(fakeProduct, 'findOneAndUpdate');
+      findOneAndUpdateStub.withArgs({ _id: fakeId }, updatedProduct).resolves(updatedProduct);
+
+      const productsController = new ProductsController(fakeProduct);
+
+      return productsController.update(request, response)
+        .then(() => {
+          sinon.assert.calledWith(response.sendStatus, 200);
+        });
+    });
+
+    context('when an error occurs', () => {
+      it('should return 422', () => {
+        const fakeId = 'a-fake-id';
+        const updatedProduct = {
+          _id: fakeId,
+          name: 'Updated product',
+          description: 'Updated description',
+          price: 150
+        };
+        const request = {
+          params: {
+            id: fakeId
+          },
+          body: updatedProduct
+        };
+        const response = {
+          send: sinon.spy(),
+          status: sinon.stub()
+        };
+
+        class fakeProduct {
+          static findOneAndUpdate() {}
+        }
+
+        const findOneAndUpdateStub = sinon.stub(fakeProduct, 'findOneAndUpdate');
+        findOneAndUpdateStub.withArgs({ _id: fakeId }, updatedProduct).rejects({ message: 'Error' });
+        response.status.withArgs(422).returns(response);
+
+        const productsController = new ProductsController(fakeProduct);
+
+        return productsController.update(request, response)
+          .then(() => {
+            sinon.assert.calledWith(response.send, 'Error');
+          });
+      });
+    });
+  });
+
+  describe('delete() product', () => {
+    it('should respond with 204 when the product has been deleted', () => {
+      const fakeId = 'a-fake-id';
+      const request = {
+        params: {
+          id: fakeId
+        }
+      };
+      const response = {
+        sendStatus: sinon.spy()
+      };
+
+      class fakeProduct {
+        static remove() {}
+      }
+
+      const removeStub = sinon.stub(fakeProduct, 'remove');
+
+      removeStub.withArgs({ _id: fakeId }).resolves([1]);
+
+      const productsController = new ProductsController(fakeProduct);
+
+      return productsController.remove(request, response)
+        .then(() => {
+          sinon.assert.calledWith(response.sendStatus, 204);
+        });
+    });
+
+    context('when an error occurs', () => {
+      it('should return 400', () => {
+        const fakeId = 'a-fake-id';
+        const request = {
+          params: {
+            id: fakeId
+          }
+        };
+        const response = {
+          send: sinon.spy(),
+          status: sinon.stub()
+        };
+
+        class fakeProduct {
+          static remove() {}
+        }
+
+        const removeStub = sinon.stub(fakeProduct, 'remove');
+
+        removeStub.withArgs({ _id: fakeId }).rejects({message: 'Error'});
+        response.status.withArgs(400).returns(response);
+
+        const productsController = new ProductsController(fakeProduct);
+
+        return productsController.remove(request, response)
+          .then(() => {
+            sinon.assert.calledWith(response.send, 'Error');
+          });
+      });
+    });
   });
 });
+
