6a71ec02ed891440155fe0664af39750208cf1d7
Waldemar Neto <waldemarnt@gmail.com>
2017-08-22 22:39:02 -0300

    Fix mongoose deprecation

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,9 @@ mongoose.Promise = Promise;
 
 const mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/test';
 
-const connect = () => mongoose.connect(mongodbUrl);
+const connect = () => mongoose.connect(mongodbUrl, {
+  useMongoClient: true
+});
 
 export default {
   connect
