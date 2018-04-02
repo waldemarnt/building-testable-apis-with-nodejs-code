475b407a0e69ba56548ab19175c3e0e42fbf90f1
Waldemar <waldemarnt@gmail.com>
2018-04-02 18:42:26 +1000

    Fix wrong merge

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import config from 'config';
 
 mongoose.Promise = Promise;
 
-const mongodbUrl = process.env.MONGODB_URL || config.get('database.mongoUrl');
+const mongodbUrl = config.get('database.mongoUrl');
 
 const connect = () => mongoose.connect(mongodbUrl, {
   useMongoClient: true
